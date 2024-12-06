8427daa616095b2288c841f628461fcb4a096d8b
mriusero <marius.ayrault@outlook.com>
2024-12-06 13:43:28 +0100

    layout restructuration

--- src/app/layouts/page4.py
+++ src/app/layouts/page4.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 def page_4():
-    st.markdown('<div class="header">#4 Statistics_</div>', unsafe_allow_html=True)
+    st.markdown('<div class="header">#4 Phase II_</div>', unsafe_allow_html=True)
     st.markdown("""
-    Empty page
+    Here is the page dedicated to the second phase of the project.
     """)
--- src/app/layouts/page6.py
+++ src/app/layouts/page6.py
@@ -1,17 +1,8 @@
 import streamlit as st
-from src.forecasting.main import handle_models
+
 
 def page_6():
-    st.markdown('<div class="header">#6 Prediction_</div>', unsafe_allow_html=True)
-    texte = """
-    Here is the Prediction phase.
-    """
-    st.markdown(texte)
-
-    #st.write('### Train DataFrame')
-    #st.dataframe(st.session_state.data.get_the('train'))
-    #st.write('### Pseudo-test DataFrame')
-    #st.dataframe(st.session_state.data.get_the('pseudo_test'))
-
-    handle_models()
-
+    st.markdown('<div class="header">#6 Empty_</div>', unsafe_allow_html=True)
+    st.markdown("""
+    Empty page
+    """)
