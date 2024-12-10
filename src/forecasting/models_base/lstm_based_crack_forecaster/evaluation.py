933033f86bc911e168dcbd3aeb5e2e36e248344b
mriusero <marius.ayrault@outlook.com>
2024-12-10 11:11:41 +0100

    Model image generation

--- src/forecasting/models_base/lstm_based_crack_forecaster/evaluation.py
+++ src/forecasting/models_base/lstm_based_crack_forecaster/evaluation.py
@@ -1,54 +1,55 @@
-import matplotlib.pyplot as plt
-import numpy as np
 import streamlit as st
-from tensorflow.keras import callbacks
+import os
+import warnings
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+import visualkeras
+from tensorflow.keras.utils import plot_model
 
 from src.forecasting.validation.display import DisplayData
 
-
-class TrainingPlot(callbacks.Callback):
-    def __init__(self, output_names):
-        super().__init__()
-        self.output_names = output_names
-        self.history = {name: {'loss': [], 'val_loss': [], 'metrics': [], 'val_metrics': []} for name in output_names}
-        self.global_history = {'loss': [], 'val_loss': []}
-        self.plot_placeholders = {name: st.empty() for name in output_names}
-
-    def on_epoch_end(self, epoch, logs={}):
-        self.global_history['loss'].append(logs.get('loss'))
-        self.global_history['val_loss'].append(logs.get('val_loss'))
-
-        for name in self.output_names:
-            self.history[name]['loss'].append(logs.get(f'{name}_loss', 0))
-            self.history[name]['val_loss'].append(logs.get(f'val_{name}_loss', 0))
-            metric_key = f'{name}_accuracy'
-            val_metric_key = f'val_{name}_accuracy'
-            self.history[name]['metrics'].append(logs.get(metric_key, 0))
-            self.history[name]['val_metrics'].append(logs.get(val_metric_key, 0))
-
-        for name in self.output_names:
-            if len(self.history[name]['loss']) > 1:
-                epochs = np.arange(0, len(self.history[name]['loss']))
-                plt.figure(figsize=(10, 5))
-                plt.plot(epochs, self.history[name]['loss'], label="Train Loss", color="blue", linestyle="-")
-                plt.plot(epochs, self.history[name]['val_loss'], label="Val Loss", color="blue", linestyle=":")
-                plt.plot(epochs, self.history[name]['metrics'], label="Train Accuracy", color="red", linestyle="-")
-                plt.plot(epochs, self.history[name]['val_metrics'], label="Val Accuracy", color="red", linestyle=":")
-                plt.title(f"Training Metrics for Output: {name} [Epoch {epoch + 1}]")
-                plt.xlabel("Epoch")
-                plt.ylabel("Loss/Accuracy")
-                plt.legend()
-
-                self.plot_placeholders[name].pyplot(plt)
-                plt.close()
+warnings.filterwarnings("ignore", message=".*legend_text_spacing_offset.*")
 
 
 def save_predictions(output_path, df, step):
-
     file_path = f"{output_path}/lstm_predictions_{step}.csv"
     df.to_csv(file_path, index=False)
-
     return f"Predictions saved successfully: {output_path}"
+
+
+def generate_model_images(model, MODEL_FOLDER):
+    """
+    Generates and displays images of the model architecture and its stylized version.
+    Parameters:
+    - model : The Keras model to visualize.
+    - MODEL_FOLDER : The folder where the images will be saved.
+    """
+    try:
+        os.makedirs(MODEL_FOLDER, exist_ok=True)
+
+        plot_model(model, to_file=f'{MODEL_FOLDER}architecture.png', show_shapes=True, show_layer_names=True)
+
+        stylized_img = visualkeras.layered_view(model, legend=True)
+        stylized_img.save(f'{MODEL_FOLDER}model_stylized.png')
+
+        fig, axs = plt.subplots(1, 2, figsize=(15, 20))
+
+        img1 = mpimg.imread(f'{MODEL_FOLDER}architecture.png')      # Display the architecture image
+        axs[0].imshow(img1)
+        axs[0].axis('off')
+        axs[0].set_title('Architecture')
+
+        img2 = mpimg.imread(f'{MODEL_FOLDER}model_stylized.png')    # Display the stylized model image
+        axs[1].imshow(img2)
+        axs[1].axis('off')
+        axs[1].set_title('Stylized Model')
+
+        st.pyplot(fig)
+
+    except Exception as e:
+        st.error('Error while generating model architecture images. '
+                 'You must install graphviz and pydot to generate the model architecture images.')
 
 
 def display_results(df):
@@ -66,3 +67,39 @@ def display_results(df):
 
     display.plot_discrete_scatter(df, 'time (months)', 'length_filtered', 'item_id')
     st.dataframe(df)
+
+    col1, col2 = st.columns([2, 2])
+
+    qualitative_pal = 'Safe'
+
+    with col1:
+        display.plot_scatter_with_color(df=df,
+                            x_col='time (months)',
+                            y_col='length_measured',
+                            color_col='Failure mode',
+                            palette_type='qualitative',
+                            palette_name=qualitative_pal)
+
+    with col2:
+        display.plot_scatter_with_color(df=df,
+                            x_col='time (months)',
+                            y_col='length_filtered',
+                            color_col='Failure mode',
+                            palette_type='qualitative',
+                            palette_name=qualitative_pal)
+
+    col1, col2 = st.columns(2)
+    with col1:
+        display.plot_histogram_with_color(df=df,
+                            x_col='time (months)',
+                            y_col='length_measured',
+                            color_col='Failure mode',
+                            palette_type='qualitative',
+                            palette_name=qualitative_pal)
+    with col2:
+        display.plot_histogram_with_color(df=df,
+                            x_col='time (months)',
+                            y_col='length_filtered',
+                            color_col='Failure mode',
+                            palette_type='qualitative',
+                            palette_name=qualitative_pal)
